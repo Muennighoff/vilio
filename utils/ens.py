f1cd19d71179ea4b045b8c01f52bf5ccd4e6798c
Muennighoff <62820084+Muennighoff@users.noreply.github.com>
2020-11-01 20:59:42 +0100

    Simplifications

--- utils/ens.py
+++ utils/ens.py
@@ -310,18 +310,24 @@ def combine_subdata(path, gt_path="./data/"):
         for i in ["ic", "tc", "oc"]:
             for csv in sorted(os.listdir(path)):
                 if (d in csv) and (i in csv):
-                    preds[d+i] = pd.read_csv(path + csv)
+                    preds[d+i] = pd.read_csv(os.path.join(path, csv))
             preds[d+i+"all"] = pd.read_json(d + "_" + i + ".jsonl", lines=True, orient="records")
     
     for d in ["dev", "test", "test_unseen"]:
-        for i in ["ic", "tc", "oc"]:
+        for i in ["ic", "tc", "oc"]: # add itc?
             for x in ["", "all"]:
                 preds[d+i+x]["proba"+i+x] = preds[d+i+x]["proba"]
                 preds[d+i+x]["proba"+i+x] = (preds[d+i+x]["proba"+i+x] - preds[d+i+x]["proba"+i+x].min())/(preds[d+i+x]["proba"+i+x].max()-preds[d+i+x]["proba"+i+x].min())
                 preds[d+i+x] = preds[d+i+x]["proba"+i+x][["id"], ["proba"+i+x]]
 
     print(preds.keys())
+    #> This func will be used both for ito optimization in the middle & at the very end based only on alls
 
+def smooth_distance(path):
+    """
+    Similar to label smoothing, smoothes the distance between predictions.
+    """
+    pass
 
 def main(path, gt_path="./data/"):
     """
