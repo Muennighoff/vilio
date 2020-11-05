f21baa54a984a7321eecba7ddf435be237ccec7a
Muennighoff <62820084+Muennighoff@users.noreply.github.com>
2020-11-05 05:56:17 +0100

    Testing

--- bash/hm_D.sh
+++ bash/hm_D.sh
@@ -23,15 +23,15 @@ python hm.py --seed 98 --model D \
 --num_features 72 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain --combine
 
 # 36 Feats, Seed 147
-cp ./data/hm_vgattr3636.tsv ./data/HM_img.tsv
+#cp ./data/hm_vgattr3636.tsv ./data/HM_img.tsv
 
-python hm.py --seed 147 --model D \
---train train --valid dev_seen --test dev_seen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
---num_features 36 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain
+#python hm.py --seed 147 --model D \
+#--train train --valid dev_seen --test dev_seen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
+#--num_features 36 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain
 
-python hm.py --seed 147 --model D \
---train traindev --valid dev_seen --test test_seen,test_unseen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
---num_features 36 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain --combine
+#python hm.py --seed 147 --model D \
+#--train traindev --valid dev_seen --test test_seen,test_unseen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
+#--num_features 36 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain --combine
 
 
 # Add EXPs to differ? 
