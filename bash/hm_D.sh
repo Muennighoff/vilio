05ac3934d3737817bf54cba883283fdbb0da5421
Muennighoff <62820084+Muennighoff@users.noreply.github.com>
2020-11-05 06:08:27 +0100

    Testing

--- bash/hm_D.sh
+++ bash/hm_D.sh
@@ -12,15 +12,15 @@ python hm.py --seed 49 --model D \
 --num_features 50 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain --combine
 
 # 72 Feats, Seed 98
-cp ./data/hm_vgattr7272.tsv ./data/HM_img.tsv
+#cp ./data/hm_vgattr7272.tsv ./data/HM_img.tsv
 
-python hm.py --seed 98 --model D \
---train train --valid dev_seen --test dev_seen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
---num_features 72 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain
+#python hm.py --seed 98 --model D \
+#--train train --valid dev_seen --test dev_seen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
+#--num_features 72 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain
 
-python hm.py --seed 98 --model D \
---train traindev --valid dev_seen --test test_seen,test_unseen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
---num_features 72 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain --combine
+#python hm.py --seed 98 --model D \
+#--train traindev --valid dev_seen --test test_seen,test_unseen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
+#--num_features 72 --loadpre ./data/pytorch_model_11.bin --contrib --midsave 2000 --subtrain --combine
 
 # 36 Feats, Seed 147
 #cp ./data/hm_vgattr3636.tsv ./data/HM_img.tsv
