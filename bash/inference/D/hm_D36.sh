4c01f289c940f68656060020860b4a4183a8ce3b
Muennighoff <62820084+Muennighoff@users.noreply.github.com>
2020-11-08 11:01:35 +0100

    Updates

--- bash/inference/D/hm_D36.sh
+++ bash/inference/D/hm_D36.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Allows for not having to copy the models to vilio/data
+loadfin=${1:-./data/LASTtrain.bin}
+loadfin2=${2:-./data/LASTtraindev.bin}
+
 # 36 Feats, Seed 147
 cp ./data/hm_vgattr3636.tsv ./data/HM_img.tsv
 
 python hm.py --seed 147 --model D \
 --test dev_seen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
---num_features 36 --loadfin ./data/LASTtrain.bin --exp D36 --subtest
+--num_features 36 --loadfin $loadfin --exp D36 --subtest
 
 python hm.py --seed 147 --model D \
 --test test_seen,test_unseen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
---num_features 36 --loadfin ./data/LASTtraindev.bin --exp D36 --subtest --combine
+--num_features 36 --loadfin $loadfin2 --exp D36 --subtest --combine
