433ba305161b1f27d81e7f42a9c9fb1ef3880f02
Muennighoff <62820084+Muennighoff@users.noreply.github.com>
2020-11-08 10:57:07 +0100

    Testing

--- bash/inference/D/hm_D36.sh
+++ bash/inference/D/hm_D36.sh
@@ -5,8 +5,8 @@ cp ./data/hm_vgattr3636.tsv ./data/HM_img.tsv
 
 python hm.py --seed 147 --model D \
 --test dev_seen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
---num_features 36 --loadfin ./data/LASTtrain.bin --contrib --exp D36 --subtest
+--num_features 36 --loadfin ./data/LASTtrain.bin --exp D36 --subtest
 
 python hm.py --seed 147 --model D \
 --test test_seen,test_unseen --lr 1e-5 --batchSize 8 --tr bert-base-uncased --epochs 5 --tsv \
---num_features 36 --loadfin ./data/LASTtraindev.bin --contrib --exp D36 --subtest --combine
+--num_features 36 --loadfin ./data/LASTtraindev.bin --exp D36 --subtest --combine
