ff4e354a706c556e31d769e7514a5405ce0d53e4
Muennighoff <62820084+Muennighoff@users.noreply.github.com>
2020-11-13 10:57:41 +0100

    Path Updates

--- ernie-vil/bash/training/EL/hm_ELVCR36.sh
+++ ernie-vil/bash/training/EL/hm_ELVCR36.sh
@@ -6,15 +6,15 @@ mv ./data/hm/hm_vgattr10100.tsv ./data/hm/HM_gt_img.tsv
 mv ./data/hm/hm_vgattr3636.tsv ./data/hm/HM_img.tsv
 
 bash run_finetuning.sh hm conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
-./data/ernielarge/params \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
+./data/ernielargevcr/params \
 train \
 2500
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_2500train \
 ./data/log \
 dev_seen ELVCR36 False
@@ -26,15 +26,15 @@ dev_seen ELVCR36 False
 # SUB 1
 
 bash run_finetuning.sh hm conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250train \
 trains1 \
 1250
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250trains1 \
 ./data/log \
 dev_seens1 ELVCR36 False
@@ -42,15 +42,15 @@ dev_seens1 ELVCR36 False
 # SUB2
 
 bash run_finetuning.sh hm conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250train \
 trains2 \
 1250
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250trains2 \
 ./data/log \
 dev_seens2 ELVCR36 False
@@ -58,15 +58,15 @@ dev_seens2 ELVCR36 False
 # SUB3
 
 bash run_finetuning.sh hm conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250train \
 trains3 \
 1250
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250trains3 \
 ./data/log \
 dev_seens3 ELVCR36 False
@@ -75,22 +75,22 @@ dev_seens3 ELVCR36 False
 
 
 bash run_finetuning.sh hm conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
-./data/ernielarge/params \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
+./data/ernielargevcr/params \
 traindev \
 2500
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_2500traindev \
 ./data/log \
 test_seen ELVCR36 False
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_2500traindev \
 ./data/log \
 test_unseen ELVCR36 False
@@ -102,22 +102,22 @@ test_unseen ELVCR36 False
 # SUB 1
 
 bash run_finetuning.sh hm conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250traindev \
 traindevs1 \
 1250
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250traindevs1 \
 ./data/log \
 test_seens1 ELVCR36 False
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250traindevs1 \
 ./data/log \
 test_unseens1 ELVCR36 False
@@ -125,22 +125,22 @@ test_unseens1 ELVCR36 False
 # SUB2
 
 bash run_finetuning.sh hm conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250traindev \
 traindevs2 \
 1250
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250traindevs2 \
 ./data/log \
 test_seens2 ELVCR36 False
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250traindevs2 \
 ./data/log \
 test_unseens2 ELVCR36 False
@@ -148,22 +148,22 @@ test_unseens2 ELVCR36 False
 # SUB3
 
 bash run_finetuning.sh hm conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250traindev \
 traindevs3 \
 1250
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250traindevs3 \
 ./data/log \
 test_seens3 ELVCR36 False
 
 bash run_inference.sh hm "" val conf/hm/model_conf_hm \
-./data/ernielarge/vocab.txt \
-./data/ernielarge/ernie_vil.large.json \
+./data/ernielargevcr/vocab.txt \
+./data/ernielargevcr/ernie_vil.large.json \
 ./output_hm/step_1250traindevs3 \
 ./data/log \
 test_unseens3 ELVCR36 True
